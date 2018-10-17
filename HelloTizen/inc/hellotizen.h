99ae03f53819970fb4880a16057af88c03130ae6
Thomas Erik Olsen <tolsen@southern.edu>
2018-10-17 18:28:51 -0400

    Initial Push - Hello Tizen

---
+++ HelloTizen/inc/hellotizen.h
@@ -0,0 +1,19 @@
+#ifndef __hellotizen_H__
+#define __hellotizen_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "hellotizen"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.hellotizen"
+#endif
+
+#endif /* __hellotizen_H__ */
